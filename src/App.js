3013942cf915abe6d461f1a709aaac765e0ca4a2
Ankush263 <ankushbanik263@gmail.com>
2022-08-04 10:56:56 +0530

    setup frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className='box flex flex-col w-full justify-center items-center gap-y-2'>
+        <input className='w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50' type="text" name="" id="" placeholder='Add your wallet Address' />
+        <input className='w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50' type="text" name="" id="" placeholder='Add collection Address' />
+        <label htmlFor="checkbox">
+          <input type="checkbox" name="" id="" />
+        </label>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}>Let's Go</button>
+      </div>
     </div>
   );
 }
