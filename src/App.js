2f58eb04eddd42d46cc28803c0d99374d8426d82
Ankush263 <ankushbanik263@gmail.com>
2022-08-04 22:27:01 +0530

    ADD fetchNFTsForCollection

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
 
   const [NFTs, setNFTs] = useState([])
 
+  const [fetchForCollection, setFetchForCollection]=useState(false)
+
   const fetchNFTs = async () => {
 
     let nfts
@@ -52,6 +54,36 @@ function App() {
 
   }
 
+  const fetchNFTsForCollection = async () => {
+
+    if(collection.length) {
+
+      let requestOptions = {
+
+        method: 'GET'
+
+      }
+
+      const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
+
+      const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${REACT_APP_API_KEY}/getNFTsForCollection/`
+
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`
+
+      const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+
+      if (nfts) {
+
+        console.log("NFTs in collection:", nfts)
+
+        setNFTs(nfts.nfts)
+
+      }
+      
+    }
+
+  }
+
 
   return (
     <div className="App flex flex-col items-center justify-center py-8 gap-y-3">
@@ -59,9 +91,10 @@ function App() {
         <input className='w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50' type="text" onChange={(e) => setWalletAddress(e.target.value)} value={wallet} placeholder='Add your wallet Address' />
         <input className='w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50' type="text" onChange={(e) => setCollectionAddress(e.target.value)} value={collection} placeholder='Add collection Address' />
         <label htmlFor="checkbox">
-          <input type="checkbox" name="" id="" />
+          Fetch for collection
+          <input type="checkbox" onChange={(e) => setFetchForCollection(e.target.checked)} />
         </label>
-        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={fetchNFTs}>Let's Go</button>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={fetchForCollection ? fetchNFTsForCollection : fetchNFTs}>Let's Go</button>
       </div>
     </div>
   );
